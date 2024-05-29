cceee3fd6fa7b2d140e4a00c94569416c157ecd3
Vânia Mendes <vaniamendes@student.dei.uc.pt>
2024-05-29 18:35:37 +0100

    login a funcionar

---
+++ proj-final-aor-frontend/src/pages/ForgetPassword.js
@@ -0,0 +1,19 @@
+
+
+import React from "react";
+import logo from '../components/assets/Logo_CSW-full-redimens.png';
+
+const Login = () => {
+    return (
+      <div>
+        <div className="header-secondary">
+          <img src={logo} alt="CSW Logo" style={{ width: '250px', height: 'auto'}}/>
+        </div>
+        <div className="login-page-container">
+          
+        </div>
+      </div>
+    );
+  };
+  
+  export default Login;
--- proj-final-aor-frontend/src/services/users.js
+++ proj-final-aor-frontend/src/services/users.js
@@ -1,8 +1,10 @@
-
+//Define the base URL for the backend API
 const url = "http://localhost:8080/project_backend/rest/users";
 
+// Export an asynchronous function named 'login' that receives a 'newUser' object as a parameter
 export async function login(newUser){
     try{
+        // Make a POST request to the 'login' endpoint of the backend API
         const response = await fetch(url + "/login",
 
         {
@@ -14,15 +16,20 @@ export async function login(newUser){
             body: JSON.stringify(newUser),
         }
         );
+        // If the response is OK, return the response text
         if(response.ok) {
-            const data = await response.json();
+            const data = await response.text();
             console.log(data);
             return data;
+            // Otherwise, return null
         } else {
+            const errorData = await response.text();
+            console.error("Login failed:", response.status, errorData);
             return null;
         }
+        // If an error occurs, throw the error
         }catch(error){
-            return error;
+            throw error;
     }
 }
 
