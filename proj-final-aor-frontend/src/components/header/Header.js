f73a112a199ab8ec3d562a7d03eb2d59a854055b
Cesar Temudo <cesarneves@student.dei.uc.pt>
2024-05-30 21:10:51 +0100

    Header with time and date

--- proj-final-aor-frontend/src/components/header/Header.js
+++ proj-final-aor-frontend/src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import logo from '../assets/Logo_CSW-full-redimens.png';
 import './Header.css';
 import {userStore} from "../../stores/UserStore";
@@ -7,6 +7,23 @@ import { IntlProvider, FormattedMessage } from "react-intl";
 
 const Header = () => {
     const {token, userData, locale, updateLocale} = userStore();
+    const [time, setTime] = useState(new Date());
+    const [selectedLanguage, setSelectedLanguage] = useState(locale);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+          setTime(new Date());
+        }, 1000);
+    
+        return () => {
+          clearInterval(timer);
+        };
+    }, []);
+
+    const handleClickLanguage = (language) => {
+        updateLocale(language);
+        setSelectedLanguage(language);
+    }
 
 
     return (
@@ -14,25 +31,24 @@ const Header = () => {
             <IntlProvider locale={locale} messages={languages[locale]}>
 
             <div className="top-header">
-                {/*<div className="time">
+                <div className="time">
                     <FormattedMessage id="time" values={{t: time}} /> 
                 </div>
                 <div className="date">
                     <FormattedMessage id="date" values={{d: Date.now()}} /> 
                 </div>
                 <div className="language-select">
-                    {["en", "pt", "fr"].map((language, index) => (
+                    {["en", "pt"].map((language, index) => (
                         <React.Fragment key={language}>
                             <span className={selectedLanguage === language ? "selected" : ""} onClick={() => handleClickLanguage(language)}>
                                 {language.toUpperCase()}
                             </span>
-                            {index !== ["en", "pt", "fr"].length - 1 && <span>|</span>}
+                            {index !== ["en", "pt"].length - 1 && <span>|</span>}
                         </React.Fragment>
                     ))}
-                </div>*/}
+                </div>
             </div>
-
-
+            <div className="bottom-header">
                 <div className="header-left">
                     <img src={logo} alt="CSW Logo" style={{ width: '200px', height: 'auto'}}/>
                     <input className="search-bar" type="search" placeholder="Search..."></input>
@@ -40,17 +56,12 @@ const Header = () => {
                 <div className="header-right">
                     <button><FormattedMessage id="login" /></button>
                     <button><FormattedMessage id="signup" /></button>
-                    <span>pt | en</span>
                 </div>
-                
-                
+            </div>
 
                 
-
-
             </IntlProvider>
-            
-
+        
             
         </header>
     );
